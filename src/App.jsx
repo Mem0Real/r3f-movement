736bcca958428d0323ba947320ba08a30a2ccfab
Mem0Real <juniourmimo@gmail.com>
2025-01-28 23:49:41 +0300

    Initial init with cube and sphere having physics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
 
 function App() {
-  return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-  );
+	return (
+		<Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
+			<color attach="background" args={["#ececec"]} />
+			<Suspense>
+				<Physics debug>
+					<Experience />
+				</Physics>
+			</Suspense>
+		</Canvas>
+	);
 }
 
 export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls, Sphere } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
 
 export const Experience = () => {
-  return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
-  );
+	return (
+		<>
+			<ambientLight intensity={0.5} />
+			<directionalLight position={[-10, 10, 0]} intensity={0.4} />
+
+			<OrbitControls />
+
+			<RigidBody position={[0, 5, 0]} colliders={"ball"}>
+				<Sphere>
+					<meshStandardMaterial color={"hotpink"} />
+				</Sphere>
+			</RigidBody>
+
+			<RigidBody position={[3, 5, 0]}>
+				<Box>
+					<meshStandardMaterial color="royalblue" />
+				</Box>
+			</RigidBody>
+
+			<RigidBody type="fixed">
+				<Box position={[0, 0, 0]} args={[10, 1, 10]}>
+					<meshStandardMaterial color="springgreen" />
+				</Box>
+			</RigidBody>
+		</>
+	);
 };
