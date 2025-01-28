f34d910ed2ef3f8e4213e363928d768b8d23c496
Mem0Real <juniourmimo@gmail.com>
2025-01-29 01:59:29 +0300

    stuck on camera control on click & drag

--- src/App.jsx
+++ src/App.jsx
@@ -21,26 +21,57 @@ function App() {
 			<Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
 				<color attach="background" args={["#ececec"]} />
 				<Suspense>
-					<Physics debug>
+					<Physics>
 						{/* <Experience /> */}
 						<Land />
 						<KeyboardControls map={keyboardMap}>
-							<Ecctrl
+							{/* <Ecctrl
 								camCollision={false} // disable camera collision detect (useless in FP mode)
 								camInitDis={-0.01} // camera intial position
 								camMinDis={-0.01} // camera zoom in closest position
+								camMaxDis={-0.01} // max camera zoom in closest position
 								camFollowMult={1000} // give a big number here, so the camera follows the target (character) instantly
 								camLerpMult={1000} // give a big number here, so the camera lerp to the followCam position instantly
 								turnVelMultiplier={1} // Turning speed same as moving speed
 								turnSpeed={100} // give it big turning speed to prevent turning wait time
 								mode={"CameraBasedMovement"} // character's rotation will follow camera's rotation in this mode
+								jumpvel={1}
+								autoBalance={false}
 							>
-								{/* This is where a character can go */}
-							</Ecctrl>
+								
+							</Ecctrl> */}
+							<Ecctrl
+								camCollision={false} // disable camera collision detect (useless in FP mode)
+								camInitDis={-0.01} // camera intial position
+								camMinDis={-0.01} // camera zoom in closest position
+								camMaxDis={-0.01} // max camera zoom in closest position
+								camFollowMult={1000} // give a big number here, so the camera follows the target (character) instantly
+								camLerpMult={1000} // give a big number here, so the camera lerp to the followCam position instantly
+								turnVelMultiplier={1} // Turning speed same as moving speed
+								turnSpeed={100} // give it big turning speed to prevent turning wait time
+								autoBalance={false}
+							/>
 						</KeyboardControls>
 					</Physics>
 				</Suspense>
 			</Canvas>
+			<div style={{ color: "rgb(0, 0, 0, 0.85)" }}>
+				<div
+					style={{
+						position: "absolute",
+						top: "10px",
+						left: "10px",
+					}}
+				>
+					<h4>W - Forward</h4>
+					<h4>A - Left</h4>
+					<h4>S - Back</h4>
+					<h4>D - Right</h4>
+				</div>
+				<div style={{ position: "absolute", top: "10px", left: "10em" }}>
+					<h4>Click & Drag for camera</h4>
+				</div>
+			</div>
 		</>
 	);
 }
