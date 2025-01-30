fa5587ef3b8837cdc8d3d1fcbe265baa5cb786f7
Mem0Real <juniourmimo@gmail.com>
2025-01-30 23:15:07 +0300

    minor lighting & font fix

--- src/components/3d-components/MyScene.jsx
+++ src/components/3d-components/MyScene.jsx
@@ -55,7 +55,7 @@ export default function MyScene(props) {
 				<Suspense fallback={null}>
 					<Physics>
 						<RigidBody type="fixed" colliders="trimesh">
-							<ambientLight intensity={1} />
+							<ambientLight intensity={0.5} />
 							<directionalLight position={[-10, 10, 0]} intensity={2} />
 							<Room2 onLoaded={() => setModelLoaded(true)} />
 						</RigidBody>
--- src/components/3d-components/Tutorial.jsx
+++ src/components/3d-components/Tutorial.jsx
@@ -3,10 +3,10 @@ export default function Tutorial({ closeTutorial }) {
 		<div className="absolute top-4 left-4 bg-neutral-400/80 min-w-64 rounded-2xl py-6 px-4">
 			<div className="relative flex justify-around gap-8">
 				<button
-					className="absolute -top-6 -right-2 px-2 rounded border cursor-pointer active:bg-neutral-200"
+					className="absolute -top-5 -right-2 px-3 rounded active:border cursor-pointer bg-black/50 text-white hover:bg-black/70"
 					onClick={closeTutorial}
 				>
-					X
+					x
 				</button>
 				<div className="flex flex-col gap-4">
 					<h1>W/up - Forward</h1>
@@ -16,9 +16,14 @@ export default function Tutorial({ closeTutorial }) {
 					<h1>Shift - Sprint</h1>
 					<h1>Space - Jump</h1>
 				</div>
-				<div className="self-start">
+				<div className="self-start flex flex-col justify-between items-center gap-4">
 					<h1>For rotation, click once then move cursor</h1>
-					<h1>To exit, press "Esc"</h1>
+					<h1>
+						To exit, press <b>"Esc"</b>
+					</h1>
+					<h1>
+						To leave the simulation, find the <b>"Exit"</b> button in the scene
+					</h1>
 				</div>
 			</div>
 		</div>
