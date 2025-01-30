d690fb00272eb2b6747743f4bcb3c6a88deba891
Mem0Real <juniourmimo@gmail.com>
2025-01-30 07:17:46 +0300

    model load first before physics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 import { Canvas } from "@react-three/fiber";
-import { Physics } from "@react-three/rapier";
+import { Physics, RigidBody } from "@react-three/rapier";
 import { Environment, KeyboardControls, Loader } from "@react-three/drei";
 
 import Ecctrl from "ecctrl";
@@ -10,6 +10,7 @@ import { Experience } from "./components/3d-components/Experience";
 import { Land } from "./components/3d-components/Land";
 import Apartment from "./components/3d-components/Apartment";
 import Tutorial from "./components/3d-components/Tutorial";
+import Room2 from "./assets/models/Room2";
 
 function App() {
 	const keyboardMap = [
@@ -20,6 +21,8 @@ function App() {
 		{ name: "jump", keys: ["Space"] },
 		{ name: "run", keys: ["Shift"] },
 	];
+	const [modelLoaded, setModelLoaded] = useState(false);
+
 	return (
 		<>
 			<Canvas
@@ -34,25 +37,32 @@ function App() {
 				{/* <color attach="background" args={["#ececec"]} /> */}
 				<Suspense fallback={null}>
 					<Physics>
-						<KeyboardControls map={keyboardMap}>
-							<Ecctrl
-								camCollision={false} // disable camera collision detect (useless in FP mode)
-								camInitDis={-0.01} // camera intial position
-								camMinDis={-0.01} // camera zoom in closest position
-								camMaxDis={-0.01} // max camera zoom in closest position
-								camFollowMult={1000} // give a big number here, so the camera follows the target (character) instantly
-								camLerpMult={1000} // give a big number here, so the camera lerp to the followCam position instantly
-								turnVelMultiplier={1} // Turning speed same as moving speed
-								turnSpeed={100} // give it big turning speed to prevent turning wait time
-								autoBalance={false}
-								// debug
-								position={[-1.5, 2, 0.05]}
-							/>
-						</KeyboardControls>
+						<RigidBody type="fixed" colliders="trimesh">
+							<Room2 onLoaded={() => setModelLoaded(true)} />
+						</RigidBody>
+
+						{modelLoaded && (
+							<KeyboardControls map={keyboardMap}>
+								<Ecctrl
+									camCollision={false} // disable camera collision detect (useless in FP mode)
+									camInitDis={-0.01} // camera intial position
+									camMinDis={-0.01} // camera zoom in closest position
+									camMaxDis={-0.01} // max camera zoom in closest position
+									camFollowMult={1000} // give a big number here, so the camera follows the target (character) instantly
+									camLerpMult={1000} // give a big number here, so the camera lerp to the followCam position instantly
+									turnVelMultiplier={1} // Turning speed same as moving speed
+									turnSpeed={100} // give it big turning speed to prevent turning wait time
+									autoBalance={false}
+									// debug
+									position={[-1.2, 1, 0.05]}
+									jumpVel={3}
+								/>
+							</KeyboardControls>
+						)}
 
 						{/* <Experience /> */}
 						{/* <Land /> */}
-						<Apartment />
+						{/* <Apartment /> */}
 					</Physics>
 				</Suspense>
 				<Environment
@@ -61,7 +71,7 @@ function App() {
 					environmentIntensity={0.5}
 				/>
 			</Canvas>
-			<Tutorial />
+			{modelLoaded && <Tutorial />}
 			<Loader />
 		</>
 	);
